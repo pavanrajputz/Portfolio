15f003f02ce2db42d833964706ef2877f4ac2de0
developer9560 <developer95608@gmail.com>
2025-01-29 00:12:53 +0530

    designing hero section and enquiry form and adding hero.js file

---
+++ hero.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById('enquiryModal');
+
+function openModal() {
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
