85a0b020cfdef07a63565eaa72ce3f9a2666a203
developer9560 <developer95608@gmail.com>
2025-01-28 22:33:44 +0530

    nothing change but box.js is added for logic

---
+++ box.js
@@ -0,0 +1,16 @@
+var hero = document.getElementsByClassName("hero");
+console.log(hero)
+
+let project_container = document.createElement("div")
+let project_box = document.createElement("div");
+let img_container = document.createElement("div");
+let img_logo = document.createElement("img");
+let msg_container = document.createElement("div");
+let a = document.createElement("a");
+let overlay = document.createElement("div")
+let p = document.createElement("p");
+project_container.classList.add("project-container");
+console.log(project_container)
+
+hero.appendChild(project_container)
+
