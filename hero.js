6ab114e3a9ea9d5d3e70d022217110ada4c868b5
developer9560 <developer95608@gmail.com>
2025-02-01 11:51:34 +0530

    button working

--- hero.js
+++ hero.js
@@ -1,8 +1,18 @@
-document.getElementById('enquiryButton').addEventListener('click', function() {
-    const Modal = document.getElementById('Modal');
-    if (Modal.style.display === 'none' || Modal.style.display === '') {
-        Modal.style.display = 'block';
-    } else {
-        Modal.style.display = 'none';
-    }
-});
+// document.getElementById('enquiryButton').addEventListener('click', function() {
+//     const Modal = document.getElementById('suraj');
+//     if (Modal.style.display === 'none' || Modal.style.display === '') {
+//         Modal.style.display = 'block';
+//     } else {
+//         Modal.style.display = 'none';
+//     }
+// });
+
+const modal = document.getElementById('suraj');
+
+function openModal() {
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
